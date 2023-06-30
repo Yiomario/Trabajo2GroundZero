f76bec4feeed93434c9e951dcd43bc3d541d4f45
INFFame <128002442+INFFame@users.noreply.github.com>
2023-06-30 11:52:11 -0400

    descarga esto mario

--- groundzero/app/admin.py
+++ groundzero/app/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 from .models import Arte, Producto, Formulario
 # Register your models here.
 
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ["nombre", "precio", "stock", "arte"]
+    list_editable = ["precio", "stock", "arte"]
+    search_fields = ["nombre"]
+    list_filter = ["arte"]
+    list_per_page = 5
+
+class FormularioAdmin(admin.ModelAdmin):
+    list_display = ["rut", "correo", "tipo_consulta"]
+    search_fields = ["rut"]
+    list_filter = ["tipo_consulta"]
+    list_per_page = 5
+
 admin.site.register(Arte)
-admin.site.register(Producto)
-admin.site.register(Formulario)
+admin.site.register(Producto, ProductoAdmin)
+admin.site.register(Formulario, FormularioAdmin)
--- groundzero/groundzero/settings.py
+++ groundzero/groundzero/settings.py
@@ -41,13 +41,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',
+    'app.apps.AppConfig',
     'colorfield',
     'crispy_forms',
     'crispy_bootstrap4',
 ]
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-X_FRAME_OPTION = 'SAMEORIGIN'
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -126,6 +126,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+import os
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
