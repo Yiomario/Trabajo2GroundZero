69b0f49e8deaa5df8cccaad83775d823a4ee7147
MacDeMarquez <ca.marqueze@duocuc.cl>
2023-06-30 19:20:50 -0400

    paginacion

--- groundzero/app/views.py
+++ groundzero/app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Producto
 from .forms import FormularioForm, ProductoForm, Producto
 from django.contrib import messages
+from django.core.paginator import Paginator
+from django.http import Http404
 
 # Create your views here.
 
@@ -68,9 +70,17 @@ def agregar_producto(request):
 
 def listar_productos(request):
     productos = Producto.objects.all()
+    page = request.GET.get('page', 1)
+
+    try:
+        paginator = Paginator(productos, 5)
+        productos = paginator.page(page)
+    except:
+        raise Http404
 
     data = {
-        'productos':productos
+        'entity':productos,
+        'paginator': paginator
     }
     return render(request, 'app/producto/listar.html', data)
 
@@ -97,8 +107,17 @@ def eliminar_producto(request, id):
 
 def galeria(request):
     productos = Producto.objects.all()
+    page = request.GET.get('page', 1)
+
+    try:
+        paginator = Paginator(productos, 3)
+        productos = paginator.page(page)
+    except:
+        raise Http404
+
     data = {
-        'productos': productos
+        'entity':productos,
+        'paginator': paginator
     }
     return render(request, 'app/galeria.html', data)
 
