96052c748a889f22c3ce85dd6bc585a64bf6dfac
Yiomario <128003036+Yiomario@users.noreply.github.com>
2023-06-30 03:57:02 -0400

    hola

--- groundzero/app/models.py
+++ groundzero/app/models.py
@@ -13,21 +13,20 @@ opciones_consultas = [
     [1, "Pinturas"  ],
     [2, "Tejidos"   ],
     [3, "Orfebreria"] 
-    ]
-    
+]
 class Producto(models.Model):
 
-    codigo = models.CharField(primary_key=True, max_length=10)
-    nombre = models.CharField(max_length=50, )
+    codigo = models.CharField(max_length=10)
+    nombre = models.CharField(max_length=50 )
     fecha_creacion = models.DateField()  
     precio = models.IntegerField()
     stock = models.IntegerField()  
     arte = models.IntegerField(choices=opciones_consultas)
     descripcion = models.CharField(max_length=42)
-    imagen = models.ImageField(upload_to="productos", null=True, blank=True)
+    imagen = models.ImageField(upload_to="productos", null=True)
 
     def __str__(self):
-        return self.nombre
+        return self.nombre    
     
 opciones_consultas = [
     [0, "Esculturas"],
@@ -37,7 +36,7 @@ opciones_consultas = [
 ]
 class Formulario(models.Model):
     rut              = models.CharField(max_length=10)
-    edad             = models.IntegerField
+    edad             = models.CharField(max_length=4)
     nombre           = models.CharField(max_length=20)
     apellidos        = models.CharField(max_length=20)
     correo           = models.EmailField()
@@ -45,6 +44,8 @@ class Formulario(models.Model):
     mensaje          = models.TextField()
     avisos           = models.BooleanField()
 
-def __str__(self):
+    def __str__(self):
         return self.nombre
+
+
     
